59fcc9f4e63320638da7e51cfbf9c4d5b24808ac
xthegoodguy <xashinx@gmail.com>
2017-08-21 21:10:22 -0500

    august 21st 1st save

---
+++ store_item.rb
@@ -0,0 +1,18 @@
+bike_1 = {type: "mountain", color: "blue", price: 100}
+bike_2 = {:type => "bmx", :color => "red", :price => 95}
+bike_3 = {type: "racing", color: "green", price: 130}
+
+# class Bike
+
+#   def initialize(input_type, input_color, input_price)
+#     @type = input_type
+#     @color = input_color
+#     @price = input_price
+#   end
+
+#   def bike_info
+#     puts "This bike is a #{bike_1[:color]} #{bike_1[:type]} bike, and it costs #{bike_1[:price]}."
+#   end
+# end
+
+# bike_1 = 
