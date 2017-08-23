907149fce429c7c3243b9ad100d18d806b2982db
xthegoodguy <xashinx@gmail.com>
2017-08-22 21:26:50 -0500

    august 22nd lesson/code

---
+++ car.rb
@@ -0,0 +1,55 @@
+class Vehicle
+  def initialize(input_options)
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  attr_reader :fuel, :drive, :model
+  attr_writer :fuel
+
+  def initialize(input_options)
+    super
+    @fuel = input_options[:fuel]
+    @drive = input_options[:drive]
+    @model = input_options[:model]
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  attr_reader :speed, :type, :weight
+
+  def initialize(input_options)
+    super
+    @speed = input_options[:speed]
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+
+car = Car.new(fuel: "18gal", drive: 2, model: "Honda")
+bike = Bike.new(speed: "20mph", type: "mountain", weight: 10)
+
+p car.fuel
--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,6 @@
-bike_1 = {type: "mountain", color: "blue", price: 100}
-bike_2 = {:type => "bmx", :color => "red", :price => 95}
-bike_3 = {type: "racing", color: "green", price: 130}
+# bike_1 = {type: "mountain", color: "blue", price: 100}
+# bike_2 = {:type => "bmx", :color => "red", :price => 95}
+# bike_3 = {type: "racing", color: "green", price: 130}
 
 # class Bike
 
@@ -10,9 +10,122 @@ bike_3 = {type: "racing", color: "green", price: 130}
 #     @price = input_price
 #   end
 
-#   def bike_info
-#     puts "This bike is a #{bike_1[:color]} #{bike_1[:type]} bike, and it costs #{bike_1[:price]}."
+#   def type
+#     return @type
 #   end
+
+#   def color
+#     return @color
+#   end
+
+#   def price=(new_price)
+#     @price = new_price
+#   end
+
+#   def price
+#     return @price
+#   end
+
+
+#   def bike_info
+#     "This bike is a #{color} #{type} bike, and it costs #{price.to_s} dollars."
+#   end
+
+#   def bike_info=
+#     "This bike is a #{color} #{type} bike, and it costs #{price.to_s} dollars."
+#   end
+
 # end
 
-# bike_1 = 
+#new session
+
+# class Bike
+
+#   attr_reader :type, :color, :price
+
+#   attr_writer :price
+
+#   def initialize(input_type, input_color, input_price)
+#     @type = input_type
+#     @color = input_color
+#     @price = input_price
+#   end
+
+
+#   def bike_info
+#     "This bike is a #{color} #{type} bike, and it costs #{price.to_s} dollars."
+#   end
+
+#   def bike_info=
+#     "This bike is a #{color} #{type} bike, and it costs #{price.to_s} dollars."
+#   end
+
+# end
+
+
+# bike_1 = Bike.new("mountain", "blue", 100)
+# bike_2 = Bike.new("bmx", "red", 95)
+# bike_3 = Bike.new("racing", "green", 130)
+
+
+# bike_1.price = 20
+# p bike_1.price
+# p bike_2.type
+# p bike_3.type
+
+#end Session
+
+class Bike
+
+  attr_reader :type, :color, :price
+
+  attr_writer :type, :color, :price
+
+  def initialize(input_options)
+    @type = input_options[:type]
+    @color = input_options[:color]
+    @price = input_options[:price]
+  end
+
+
+  def bike_info
+    "This bike is a #{color} #{type} bike, and it costs #{price.to_s} dollars."
+  end
+
+end
+
+class Food < Bike
+  attr_reader :shelf_life
+  attr_writer :shelf_life
+
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
+
+bike_1 = Bike.new(  
+                  type: "mountain",
+                  color: "blue",
+                  price: 100
+                  )
+bike_2 = Bike.new(
+                  type: "bmx",
+                  color: "red",
+                  price: 95
+                  )
+bike_3 = Bike.new(
+                  type: "racing",  
+                  color: "green", 
+                  price: 130
+                  )
+
+food = Food.new(
+                  shelf_life: "August 22nd"
+                  )
+# The "<" sign in classes points to a different class
+
+p food.shelf_life
+
+
